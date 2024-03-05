c9d0552873db712c942b97ad3a8908f4383c8bc0
Vinícius Campos <vpcmps@gmail.com>
2024-03-05 20:47:05 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"github.com/vpcmps/gopportunities/config"
 	"github.com/vpcmps/gopportunities/router"
 )
 
+var (
+	logger = config.NewLogger("")
+)
+
 func main() {
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing config: %s", err.Error())
+		return
+	}
 	//Initialize the router
 	router.Initialize()
 }
