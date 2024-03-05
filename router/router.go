5141ef77a7f1c7a94c571db59a920b316ddcdfe8
Vinícius Campos <vpcmps@gmail.com>
2024-03-05 16:36:54 -0300

    feat: add oppening routes

--- router/router.go
+++ router/router.go
@@ -5,10 +5,9 @@ import "github.com/gin-gonic/gin"
 func Initialize() {
 	// Code to initialize the router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// Initialize routes
+	initializeRoutes(router)
+
 	router.Run()
 }
