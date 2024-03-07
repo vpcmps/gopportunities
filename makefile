16fb8b43487d40ea99edef4d7e9475831f787245
Vinícius Campos <vpcmps@gmail.com>
2024-03-07 17:10:37 -0300

    feat: add makefile and swagger documentation

---
+++ makefile
@@ -0,0 +1,16 @@
+.PHONY: default run build test docs clean
+
+APP_NAME=gopportunities
+
+default: run
+run:
+	@go run main.go
+build:
+	@go build -o $(APP_NAME) main.go
+test:
+	@go test -v ./...
+docs:
+	@swag init
+clean:
+	@rm -f $(APP_NAME)
+	@rm -f ./docs
--- router/routes.go
+++ router/routes.go
@@ -2,11 +2,19 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/vpcmps/gopportunities/docs"
 	"github.com/vpcmps/gopportunities/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
+
+	docs.SwaggerInfo.Title = "Gopportunities"
+	docs.SwaggerInfo.Description = "Gopportunities API"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.BasePath = "/api/v1"
 
 	v1 := router.Group("/api/v1")
 	{
@@ -17,4 +25,5 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 	}
 
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
